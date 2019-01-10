39ea26dfc716a66d821be365e3a3053f94198b7e
JinleeJeong <pjjr0118@gmail.com>
2019-01-10 16:24:07 +0900

    Updates

---
+++ precache-manifest.69c9b75c0b8e9d8d271467e779fdd8a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "8e0ba81600326842bce3",
+    "url": "/movie_app/static/js/main.8e0ba816.chunk.js"
+  },
+  {
+    "revision": "22fdec2062f68dfa3e16",
+    "url": "/movie_app/static/js/1.22fdec20.chunk.js"
+  },
+  {
+    "revision": "8e0ba81600326842bce3",
+    "url": "/movie_app/static/css/main.5cd34cf4.chunk.css"
+  },
+  {
+    "revision": "846446c4d1237a25ba6224d3ec4bded1",
+    "url": "/movie_app/index.html"
+  }
+];
