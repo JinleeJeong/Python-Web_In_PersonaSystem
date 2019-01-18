1da86593bd531ec57f0b1d054a5661cd8fb89ea2
JinleeJeong <pjjr0118@gmail.com>
2019-01-18 17:39:29 +0900

    19/01/18 Bitnami My_Project in php

---
+++ Fifth_Weekend/php_Myproject_Bitnami/MY_Project/index.php
@@ -0,0 +1,101 @@
+<!doctype html>
+<html lang="ko">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link href="css/bootstrap.min.css?ver=1" rel="stylesheet">
+    <link rel="stylesheet" href="style.css?ver=1">
+    <title>Hello, world!</title>
+  </head>
+  <body>
+  <div class="row">
+    <div class="col-md-1"><img class="logo" src="static/Logo.jpg" alt="Logo"></div>
+
+    <div class="col-md-7">
+      <ul>
+        <li class ="top_nav"><a href="#">LINE UP</a></li>
+        <li class ="top_nav"><a href="#">ABOUT US</a></li>
+        <li class ="top_nav"><a href="#">TICKETING</a></li>
+        <li class ="top_nav"><a href="#">ADDRESS</a></li>
+        <li class ="top_nav"><a href="#">CONTACT US</a></li>
+      </ul>
+    </div>
+    <div class ="col-md-2 col-md-offset-2">
+        <ul class="nav nav-pills">
+          <li role="presentation" class = "active"><a href="#">유튜브</a></li>
+          <li role="presentation"><a href="#">페이스북</a></li>
+          <li role="presentation"><a href="#">트위터</a></li>
+        </ul>
+    </div>
+
+  </div>
+
+  <div class="padding">
+
+  </div>
+
+
+
+
+  <div class="container-fluid">
+
+    <h2>ABOUT US</h2>
+
+      <div class="about_us">
+        <p><a href="#">ALL</a></p>
+        <p><a href="#">인사말&nbsp&nbsp&nbsp<span class = "glyphicon glyphicon-chevron-right"></a></p>
+        <p><a href="#">히스토리<span class = "glyphicon glyphicon-chevron-right"></a></p>
+        <p><a href="#">오시는길<span class = "glyphicon glyphicon-chevron-right"></a></p>
+      </div>
+      <div class="container_year">
+
+        <div class="btn-group-vertical" role="group" aria-label="Vertical button group">
+            <p class = "glyphicon glyphicon-chevron-up"></p>
+            <button type="button" class="btn btn-default">2018</button>
+            <button type="button" class="btn btn-default">2017</button>
+            <button type="button" class="btn btn-default">2015</button>
+            <button type="button" class="btn btn-default">2014</button>
+            <button type="button" class="btn btn-default">2013</button>
+            <button type="button" class="btn btn-default">2012</button>
+            <p class = "glyphicon glyphicon-chevron-down"></p>
+        </div>
+      </div>
+      <div class="container_content">
+          <p class = "content">소영이 언능 일어나서 씻어야지!</p>
+          <div class="content_bottom">
+            <div class="content_bottom_left">
+                left
+            </div>
+            <div class="content_bottom_right">
+                right
+            </div>
+          </div>
+
+
+
+      </div>
+  </div>
+
+
+
+
+
+
+  <div class="footer">
+    plz jazz with me
+  </div>
+
+
+
+
+
+
+  <!-- jQuery (부트스트랩의 자바스크립트 플러그인을 위해 필요합니다) -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+  <!-- 모든 컴파일된 플러그인을 포함합니다 (아래), 원하지 않는다면 필요한 각각의 파일을 포함하세요 -->
+  <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
